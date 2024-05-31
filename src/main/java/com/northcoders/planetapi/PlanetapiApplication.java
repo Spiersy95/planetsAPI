8e1cd941e186807fedda48e831353ad7877c4b32
Spiersy95 <scottspeirsstirling@gmail.com>
2024-05-31 14:43:12 +0100

    First commit

---
+++ src/main/java/com/northcoders/planetapi/PlanetapiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.planetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/planetapi/PlanetapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.planetapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
